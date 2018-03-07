ff190eaf8ea68c6bffd4ea1299ed9df9419bfdeb
GiangLe <giangle@Macbooks-MacBook-Pro.local>
2018-03-07 10:15:22 +0700

    Initial Commit

---
+++ Link Desk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Link Desk
+//
+//  Created by GiangLe on 3/7/18.
+//  Copyright © 2018 GiangLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Link Desk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Link Desk
+//
+//  Created by GiangLe on 3/7/18.
+//  Copyright © 2018 GiangLe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
